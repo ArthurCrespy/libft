c49fe736f92e665ee27e4fa13ca9fac0e58d83dd
CRESPY Arthur <arthur@crespy.eu>
2022-11-15 08:21:06 +0100

    Add the header

---
+++ libft.h
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 13:30:14 by acrespy           #+#    #+#             */
+/*   Updated: 2022/11/15 07:56:36 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+# include <stddef.h>
+# include <string.h>
+# include <ctype.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <bsd/bsd.h>
+# include <unistd.h>
+
+int				ft_atoi(const char *str);
+int				ft_isspace(char c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+int				ft_strcmp(char *s1, char *s2);
+int				ft_tablen(char **strs, int size);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+int				ft_isincharset(char const *s, char c);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memset(void *s, int c, size_t n);
+void			*ft_calloc(size_t nmemb, size_t size);
+void			*ft_memchr(const void *s, int c, size_t n);
+void			*ft_memcpy(void *dest, const void *src, size_t n);
+void			*ft_memmove(void *dest, const void *src, size_t n);
+void			ft_striteri(char *s, void (*f)(unsigned int, char*));
+void			ft_putnbr_fd(int n, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putchar_fd(char c, int fd);
+void			ft_putendl_fd(char *s, int fd);
+char			*ft_itoa(int n);
+char			*ft_strdup(const char *s);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strcat(char *dest, char *src);
+char			*ft_strcpy(char *dest, char *src);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strtrim(char const *s1, char const *set);
+char			*ft_strnstr(const char *big, const char *little, size_t len);
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char			**ft_split(char const *s, char c);
+size_t			ft_strlen(const char *s);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+
+#endif
