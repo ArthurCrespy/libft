4754325ee0d90098964c99223230ab7e4f2e9b6b
CRESPY Arthur <arthur@crespy.eu>
2022-11-21 15:41:29 +0100

    Update main.c and his lib

--- files/main.c
+++ files/main.c
@@ -88,6 +88,10 @@ int	main (void)
  	printf("itoa    - %s | -\n", ft_itoa(-123156));
 	printf("-------- Results -------- \n");*/
 
+	char * str = strdup("0123456789");
+    char * s = ft_substr(str, 9, 10);
+	printf("%s\n", s);
+
 	//End
 	return (0);
 }
