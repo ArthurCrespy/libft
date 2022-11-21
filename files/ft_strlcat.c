c676b3e48d16842d90c16c6658cb3c15122f18a3
CRESPY Arthur <arthur@crespy.eu>
2022-11-21 18:43:43 +0100

    Fix issues on strlcat

--- files/ft_strlcat.c
+++ files/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:14:52 by acrespy           #+#    #+#             */
-/*   Updated: 2022/11/15 07:53:56 by acrespy          ###   ########.fr       */
+/*   Updated: 2022/11/21 18:34:02 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,21 @@
 
 size_t	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i_src;
-	unsigned int	i_dest;
-	unsigned int	length_src;
-	unsigned int	length_dest;
+	size_t	i;
+	size_t	j;
 
-	i_src = 0;
-	i_dest = ft_strlen(dest);
-	length_src = ft_strlen(src);
-	length_dest = ft_strlen(dest);
-	while (src[i_src] != 0x00 && i_dest + 1 < size)
+	i = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	while (dest[i] != '\0' && i < size)
+		i++;
+	j = i;
+	while (src[i - j] != '\0' && i < size - 1)
 	{
-		dest[i_dest] = src[i_src];
-		i_dest++;
-		i_src++;
+		dest[i] = src[i - j];
+		i++;
 	}
-	dest[i_dest] = 0x00;
-	if (size > length_dest)
-		return (length_dest + length_src);
-	return (length_src + size);
+	if (j < size)
+		dest[i] = '\0';
+	return (j + ft_strlen(src));
 }
