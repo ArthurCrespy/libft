7c6d2f231e46ed2bbd0ac08904f5bee0ad3b7ccb
CRESPY Arthur <arthur@crespy.eu>
2022-11-21 15:06:20 +0100

    Fix lstmap

--- files/ft_lstmap_bonus.c
+++ files/ft_lstmap_bonus.c
@@ -12,25 +12,24 @@
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new;
-	t_list	*tmp;
+	t_list	*lst_map;
+	t_list	*new_node;
 
-	if (!lst || !f)
-		return (0x00);
-	new = f(lst);
-	tmp = new;
-	while (lst->next)
+	lst_map = NULL;
+	while (lst)
 	{
-		lst = lst->next;
-		tmp->next = f(lst);
-		if (!tmp->next)
+		new_node = ft_lstnew(f(lst->content));
+		if (new_node == NULL)
 		{
-			free(tmp->next);
-			return (0x00);
+			ft_lstclear(&lst_map, del);
+			return (NULL);
 		}
-		tmp = tmp->next;
+		ft_lstadd_back(&lst_map, new_node);
+		if (lst_map == NULL)
+			return (NULL);
+		lst = lst->next;
 	}
-	return (new);
+	return (lst_map);
 }
