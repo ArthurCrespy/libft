136e4fd3767c1198547d26fcfc7748205f875f6e
CRESPY Arthur <arthur@crespy.eu>
2022-11-21 18:43:27 +0100

    Fix issues on while loop

--- files/ft_memcmp.c
+++ files/ft_memcmp.c
@@ -23,7 +23,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (i < n)
+	while (i < n - 1)
 	{
 		if (str1[i] == 0 && str2[i] == 0 && i + 1 == n)
 			return (0);
