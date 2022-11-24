c254a833e8181186c3d13418524feb694dea0c75
CRESPY Arthur <arthur@crespy.eu>
2022-11-24 13:22:24 +0100

    Fix Norminette issues

--- files/main.c
+++ files/main.c
@@ -6,12 +6,13 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 13:31:58 by acrespy           #+#    #+#             */
-/*   Updated: 2022/11/09 19:43:00 by acrespy          ###   ########.fr       */
+/*   Updated: 2022/11/24 13:16:47 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	main (void) {
+int	main (void)
+{
 	return (0);
 }
