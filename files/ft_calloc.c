3e9eb8b65e58338eb70899249a73c4f1a2f6d1f7
CRESPY Arthur <arthur@crespy.eu>
2022-11-24 18:44:50 +0100

    Improve overflow check

--- files/ft_calloc.c
+++ files/ft_calloc.c
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*result;
 
-	if (size != 0 && (nmemb * size) / size != nmemb)
+	if ((size != 0 && nmemb != 0) && ((nmemb * size) / size != nmemb))
 		return (NULL);
 	if (nmemb == 0 || size == 0)
 		return (malloc(0));
